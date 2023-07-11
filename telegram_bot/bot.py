02c8dbc5ec005078c47fc32528ebf2c3e5f59789
Nemesis <lorenzo.balugani@gmail.com>
2023-07-11 18:53:38 +0200

    :sparkles: Some work on bot

--- telegram_bot/bot.py
+++ telegram_bot/bot.py
@@ -1,29 +1,30 @@
 import telebot
 import json
 import requests
-from configuration import BOT_TOKEN, ADMIN_USERNAME, ADMIN_PASSWORD, CHAT_ID
+from configuration import BOT_TOKEN, ADMIN_USERNAME, ADMIN_PASSWORD, CHAT_ID, API_URI
 from telegram_menu import BaseMessage, TelegramMenuSession, NavigationHandler
-
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
-#defining settings controllable by the bot
-litter_settings = '{"sampling_rate": {"value": 1000}, "use_counter": {"value": 3}, "used_offset": {"value": 5}, "tare_timeout": {"value": 10}, "danger_threshold": {"value": 1}, "danger_counter": {"value": 1}}}'
-settings=json.loads(litter_settings)
+
+# defining settings controllable by the bot
+
+def login():
+    ans = requests.post(f"http://{API_URI}/token",
+                        data={"username": ADMIN_USERNAME, "password": ADMIN_PASSWORD, "grant_type": "password"})
+    ans = ans.json()
+    return f"Bearer {ans['access_token']}"
 
 
+settings = requests.get(f'http://{API_URI}/api/settings/v1', headers={"Authorization": login()})
+settings = settings.json()
 
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    if str(message.chat.id) != CHAT_ID:
-        return
-    bot.reply_to(message, "Hi, PurrfectPotty bot here. You can use me to receive notifications about the status of "
-                          "your cat's litterbox, or change parameters of the smart litterbox.")
 
 def send_sampling_rate(message):
     if str(message.chat.id) != CHAT_ID:
         return
     bot.reply_to(message, "Current sampling rate is " + str(settings['sampling_rate']['value']) + " seconds.")
+
 
 class StartMessage(BaseMessage):
     """Start menu, create all app sub-menus."""
@@ -39,6 +40,7 @@ class StartMessage(BaseMessage):
     def update(self) -> str:
         """Update message content."""
         return "Hello, world!"
+
 
 class SecondMenuMessage(BaseMessage):
     """Second menu, create an inlined button."""
@@ -75,116 +77,109 @@ class SecondMenuMessage(BaseMessage):
         return ":warning: Second message"
 
     @staticmethod
-    def set_sampling_rate(message) -> str:
-        if message.text.isdigit() == False:
+    def set_sampling_rate(self, message) -> str:
+        if not message.text.isdigit():
             bot.reply_to(message, "Sampling rate must be a number.")
             return
-        requests.post('http://API_URI:5000/set_sampling_rate', json={"sampling_rate": message.text})
+        requests.post(f'http://{API_URI}/set_sampling_rate', headers={"Authorization": login()}, json={"sampling_rate": message.text})
         settings['sampling_rate']['value'] = message.text
         return "Sampling rate set to " + message.text + " seconds."
-    
-    def set_tare_timeout(message)-> str:
-        if message.text.isdigit() == False:
+
+    def set_tare_timeout(self, message) -> str:
+        if not message.text.isdigit():
             bot.reply_to(message, "Setting value must be a number.")
-            return
-        json_info = requests.get('http://API_URI:5000/api/settings/v1')
+            return ""
+        json_info = requests.get(f'http://{API_URI}/api/settings/v1', headers={"Authorization": login()})
         json_info['litter']['tare_timeout']['value'] = message.text
-        requests.post('http://API_URI:5000/api/settings/v1', json=json_info)
+        requests.post(f'http://{API_URI}/api/settings/v1', headers={"Authorization": login()}, json=json_info)
         settings['tare_timeout']['value'] = message.text
         return "Tare timeout set to " + message.text + " seconds."
-    
-    def set_danger_threshold(message)-> str:
-        if message.text.isdigit() == False:
+
+    def set_danger_threshold(self, message) -> str:
+        if not message.text.isdigit():
             bot.reply_to(message, "Setting value must be a number.")
             return
-        json_info = requests.get('http://API_URI:5000/api/settings/v1')
+        json_info = requests.get(f'http://{API_URI}/api/settings/v1', headers={"Authorization": login()})
         json_info['litter']['danger_threshold']['value'] = message.text
-        requests.post('http://API_URI:5000/api/settings/v1', json=json_info)
+        requests.post(f'http://{API_URI}/api/settings/v1', headers={"Authorization": login()}, json=json_info)
         settings['danger_threshold']['value'] = message.text
         return "Danger threshold set to " + message.text + " grams."
-    
-    def set_danger_counter(message)-> str:
-        if message.text.isdigit() == False:
+
+    def set_danger_counter(self, message) -> str:
+        if not message.text.isdigit():
             bot.reply_to(message, "Setting value must be a number.")
             return
-        json_info = requests.get('http://API_URI:5000/api/settings/v1')
+        json_info = requests.get(f'http://{API_URI}/api/settings/v1', headers={"Authorization": login()})
         json_info['litter']['danger_counter']['value'] = message.text
-        requests.post('http://API_URI:5000/api/settings/v1', json=json_info)
+        requests.post(f'http://{API_URI}/api/settings/v1', headers={"Authorization": login()}, json=json_info)
         settings['danger_counter']['value'] = message.text
         return "Danger counter set to " + message.text + " times."
-    
-    def set_use_counter(message)-> str:
-        if message.text.isdigit() == False:
+
+    def set_use_counter(self, message) -> str:
+        if not message.text.isdigit():
             bot.reply_to(message, "Setting value must be a number.")
             return
-        json_info = requests.get('http://API_URI:5000/api/settings/v1')
+        json_info = requests.get(f'http://{API_URI}/api/settings/v1', headers={"Authorization": login()})
         json_info['litter']['use_counter']['value'] = message.text
-        requests.post('http://API_URI:5000/api/settings/v1', json=json_info)
+        requests.post(f'http://{API_URI}/api/settings/v1', headers={"Authorization": login()}, json=json_info)
         settings['use_counter']['value'] = message.text
         return "Use counter set to " + message.text + " times."
-    
-    def set_used_offset(message)-> str:
-        if message.text.isdigit() == False:
+
+    def set_used_offset(self, message) -> str:
+        if not message.text.isdigit():
             bot.reply_to(message, "Setting value must be a number.")
             return
-        json_info = requests.get('http://API_URI:5000/api/settings/v1')
+        json_info = requests.get(f'http://{API_URI}/api/settings/v1', headers={"Authorization": login()})
         json_info['litter']['used_offset']['value'] = message.text
-        requests.post('http://API_URI:5000/api/settings/v1', json=json_info)
+        requests.post(f'http://{API_URI}/api/settings/v1', headers={"Authorization": login()}, json=json_info)
         settings['used_offset']['value'] = message.text
         return "Used offset set to " + message.text + " grams."
-    
-    def run_and_notify(action: str) -> str:
-        #take user input from telegram message interface
+
+    def run_and_notify(self, message, action: str) -> str:
+        # take user input from telegram message interface
         # send the input to the API
         # update the settings dictionary
         # return a string to be sent as a message
         message = message.text
         msg = message.text.isdigit()
-        if msg == False:
+        if not msg:
             bot.reply_to(message, "Sampling rate must be a number.")
-            return 
+            return
+        if not message.text.isdigit():
+            bot.reply_to(message, "Setting value must be a number.")
+            return
         if action == "sampling_rate":
-            return set_sampling_rate(message)
+            return self.set_sampling_rate(message)
         elif action == "tare_timeout":
-            return set_tare_timeout(message)
+            return self.set_tare_timeout(message)
         elif action == "danger_threshold":
-            return set_danger_threshold(message)
+            return self.set_danger_threshold(message)
         elif action == "danger_counter":
-            return set_danger_counter(message)
+            return self.set_danger_counter(message)
         elif action == "use_counter":
-            return set_use_counter(message)
+            return self.set_use_counter(message)
         elif action == "used_offset":
-            return set_used_offset(message)
+            return self.set_used_offset(message)
         else:
             return "Error"
 
-
-
-    
-        if message.text.isdigit() == False:
-            bot.reply_to(message, "Sampling rate must be a number.")
-            return
-        requests.post('http://API_URI:5000/set_used_offset', json={"used_offset": message.text})
-        settings['used_offset']['value'] = message.text
-        return "Used offset set to " + message.text + " grams."
-    
-    def get_info(message) -> str:
+    def get_info(self, message) -> str:
         # get the info from the API
-        json_info = requests.get('http://API_URI:5000/api/settings/v1')
-        textualized_info = "Current settings:\n", "Sampling rate: ", 
-        json_info['sampling_rate']["value"], " ms\n", 
-        "Tare timeout: ", json_info['tare_timeout']["value"], 
-        " seconds\n", "Danger threshold: ", json_info['danger_threshold']["value"], " grams\n", 
-        "Danger counter: ", json_info['danger_counter']["value"], " times\n", 
-        "Use counter: ", json_info['use_counter']["value"], " times\n", 
-        "Used offset: ", json_info['used_offset']["value"], " grams\n"
+        json_info = requests.get(f'http://{API_URI}/api/settings/v1', headers={"Authorization": login()})
+        textualized_info = ("Current settings:\n", "Sampling rate: ",
+                            json_info['sampling_rate']["value"], " ms\n",
+                            "Tare timeout: ", json_info['tare_timeout']["value"],
+                            " seconds\n", "Danger threshold: ", json_info['danger_threshold']["value"], " grams\n",
+                            "Danger counter: ", json_info['danger_counter']["value"], " times\n",
+                            "Use counter: ", json_info['use_counter']["value"], " times\n",
+                            "Used offset: ", json_info['used_offset']["value"], " grams\n")
         # return a string to be sent as a message
-        bot.reply_to(message, textualized_info)
-        return textualized_info
+        txt = ""
+        for elem in textualized_info:
+            txt += elem
+        bot.reply_to(message, txt)
+        return txt
 
 
 print("Bot is now running...")
-bot.infinity_polling()
 TelegramMenuSession(BOT_TOKEN).start(StartMessage)
-
-
