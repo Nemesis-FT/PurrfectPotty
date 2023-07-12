eb4c66dd0a5ad81dab0597674e54a38f3d708b64
Nemesis <lorenzo.balugani@gmail.com>
2023-07-12 18:46:21 +0200

    :sparkles: Moved config file

--- data_proxy/backend/web/utils.py
+++ data_proxy/backend/web/utils.py
@@ -13,13 +13,13 @@ influx_client = influxdb_client.InfluxDBClient(
 
 def get_current_config():
     data = None
-    with open("sensor_config.json") as file:
+    with open("../../../sensor_config.json") as file:
         data = json.load(file)
     return data
 
 
 def save_new_config(config):
-    with open("sensor_config.json", "w") as file:
+    with open("../../../sensor_config.json", "w") as file:
         json.dump(config, file)
 
 
